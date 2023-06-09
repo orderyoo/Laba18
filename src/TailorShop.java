ccbbaabaaf6cfb3ac671cec56ef4eb3a5a2f1a1e
orderyoo <122743400+orderyoo@users.noreply.github.com>
2023-06-09 08:19:50 +0300

    Update TailorShop.java
    убрал лишнее

--- src/TailorShop.java
+++ src/TailorShop.java
@@ -3,20 +3,16 @@ public class TailorShop {
     public void dressWomen(Clothing[] clothes) {
         System.out.println("Женская одежда:");
         for (Clothing clothing : clothes) {
-            if (clothing instanceof WomenClothing) {
-                System.out.println("Размер: " + clothing.getSize() + ", Цена: $" + clothing.getCost() + ", Цвет: " + clothing.getColor());
+             System.out.println("Размер: " + clothing.getSize() + ", Цена: $" + clothing.getCost() + ", Цвет: " + clothing.getColor());
                 ((WomenClothing) clothing).dressWomen();
-            }
         }
     }
 
     public void dressMen(Clothing[] clothes) {
         System.out.println("Мужская одежда:");
         for (Clothing clothing : clothes) {
-            if (clothing instanceof MenClothing) {
-                System.out.println("Размер: " + clothing.getSize() + ", Цена: $" + clothing.getCost() + ", Цвет: " + clothing.getColor());
+             System.out.println("Размер: " + clothing.getSize() + ", Цена: $" + clothing.getCost() + ", Цвет: " + clothing.getColor());
                 ((MenClothing) clothing).dressMen();
-            }
         }
     }
 }
